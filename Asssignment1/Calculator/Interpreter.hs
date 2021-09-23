e7098c7182d0db60ef93e356d5289d54d7c9ac7c
Madeleine Gradney <40253828+gradneymadeleine@users.noreply.github.com>
2021-09-22 22:34:39 -0700

    Add files via upload

--- Asssignment1/Calculator/Interpreter.hs
+++ Asssignment1/Calculator/Interpreter.hs
@@ -4,6 +4,16 @@ import AbsNumbers
 
 eval :: Exp -> Integer
 eval (Num n) = n
+
 eval (Plus n m) = (eval n) + (eval m)
 eval (Minus n m) = (eval n) - (eval m)
+
 eval (Times n m) = (eval n) * (eval m)
+eval (Div n m) = div (eval n) (eval m)
+
+eval (Min n m) | (eval n) > (eval m) = (eval m)
+| (eval n) < (eval m) = (eval n)
+eval (Max n m) | (eval n) > (eval m) = (eval n)
+| (eval n) < (eval m) = (eval m)
+
+eval (Mod n m) = mod (eval n) (eval m)
