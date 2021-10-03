38b79e3681260b049171a1173ff2b093c6c3fc10
Madeleine Gradney <40253828+gradneymadeleine@users.noreply.github.com>
2021-10-03 16:27:46 -0700

    Add files via upload

--- arithmetic.hs
+++ arithmetic.hs
@@ -9,7 +9,7 @@ data NN = O | S NN
   deriving (Eq,Show) -- for equality and printing
 
 -- Integers
-data II = II NN NN
+data II = II NN NN -- II is subtraction of 2 NN numbers
   deriving (Eq,Show) -- for equality and printing
 
 -- Positive integers (to avoid dividing by 0)
@@ -60,6 +60,22 @@ modN n m | minN n m==False = modN m (subtrN n m) | minN n m==True = modN n (subt
 -- II Arithmetic
 ----------------
 
+-- Addition: (a-b)+(c-d)=(a+c)-(b+d)
+addI :: II -> II -> II
+addI (II a b) (II c d) = II (addN a c) (addN b d)
+
+-- Multiplication: (a-b)*(c-d)=(ac+bd)-(ad+bc)
+multI :: II -> II -> II
+multI (II a b) (II c d) = II (addN (multN a c) (multN b d)) (addN (multN a d) (multN b c))
+
+-- Subtraction: (a-b)-(c-d)=(a+d)-(b+c)
+subtrI :: II -> II -> II
+subtrI (II a b) (II c d) = II (addN a d) (addN b c)
+
+-- Negation: -(a-b)=(b-a)
+negI :: II -> II
+negI (II a b) = II b a
+
 ----------------
 -- QQ Arithmetic
 ----------------
@@ -80,11 +96,11 @@ multP :: PP -> PP -> PP
 multP I n = n
 multP (T n) m = addP m (multP n m)
 
--- divide positive numbers
-divP :: NN -> PP -> NN
-divP n m = if n == (p2n m) then (S O)
-else if (modN n(p2n m)) then O
-else (addN (S O) (divP (subtrP n m)m))
+-- -- divide positive numbers
+-- divP :: NN -> PP -> NN
+-- divP n m = if n == (pp_nn m) then (S O)
+-- else if (modN n(pp_nn m) == O) then O -- "== O" needs to be tested
+-- else (addN (S O) (divP (subtrP n m)m))
 
 -- minimum positive numbers
 minP :: PP -> PP -> Bool
@@ -92,20 +108,29 @@ minP n I = False -- n ≠ or > 0
 minP I n = True -- I < 1-- I < 1
 minP (T n) (T m ) = minP n m
 
--- convert PP to NN
-p2n :: PP -> NN
-p2n I = (S O)
-p2n (T n) = S(p2n n)
+-- -- Addition: (a/b)+(c/d)=(ad+bc)/(bd)
+addQ :: QQ -> QQ -> QQ
+addQ (QQ a b) (QQ c d) = QQ (addI(multI(a pp_ii(d))) (multI(c pp_ii(b)))) (multI(b pp_ii(d)))
 
--- convert NN to PP
-n2p :: NN -> PP
-n2p 0 = error "0 ≠ positive number"
-n2p (S O)= I
-n2p (S n) = T(n2p n)
+-- addQ QQ (a b)(c d) = divQ (addP (multP a d) (multP b c)) (multP b d)
+
+-- Multiplication: (a/b)*(c/d)=(ac)/(bd)
+multQ :: QQ -> QQ -> QQ
+multQ (QQ a b) (QQ c d) = QQ (multI a c) (multP b d)
 
 ----------------
 -- Normalisation
 ----------------
+
+-- Normalisation of II->II
+normalizeI :: II -> II
+normalizeI (II n O) = (II n O)
+normalizeI (II O n) = (II O n)
+normalizeI (II (S n) (S m)) = normalizeI (II n m)
+
+-- Normalization by evaulation
+nbv :: II -> II
+nbv (II n m) = II (nn_int(int_nn(n) - int_nn(m))) (O)
 
 -- fractions
 -- non-negative
@@ -115,10 +140,82 @@ type Frac = (NN, PP)
 -- Converting between VM-numbers and Haskell-numbers
 ----------------------------------------------------
 
+-- convert numbers of type PP to numbers of type II
+-- ii_pp :: PP -> II
+-- ii_pp I = II (S O) O
+-- ii_pp (T n) = addI (ii_pp n) (ii_pp I)
+
+pp_ii :: PP -> II
+pp_ii I = II (S O) O
+pp_ii (T n) = addI (II (S O) O) (pp_ii n)
+
+-- convert PP to NN
+pp_nn :: PP -> NN
+pp_nn I = (S O)
+pp_nn (T n) = S (pp_nn n)
+
+-- convert NN to PP
+nn_pp :: NN -> PP
+nn_pp O = error "0 ≠ positive number"
+nn_pp (S O) = I
+nn_pp (S n) = T (nn_pp n)
+
+-- Precondition: Inputs are non-negative
+
+nn_int :: Integer -> NN
+nn_int 0 = O
+nn_int n = S (nn_int (n - 1))
+
+int_nn :: NN -> Integer
+int_nn O = 0
+int_nn (S n) = 1 + int_nn n
+
+ii_int :: Integer -> II
+ii_int 0 = II (O) (O)
+ii_int n = II  (nn_int(n)) (O)
+
+int_ii :: II -> Integer
+int_ii (II O O) = 0
+int_ii (II (n) m) = int_nn(n) - int_nn(m)
+
+-- Precondition: Inputs are positive
+
+pp_int :: Integer -> PP
+pp_int 1 = I
+pp_int n = T (pp_int (n - 1))
+
+int_pp :: PP->Integer
+int_pp I = 1
+int_pp (T n) = (int_pp n) + 1
+
+-- float_qq :: QQ -> Float
+-- float_qq 
+
+
+
 ----------
 -- Testing
 ----------
 main = do
-    print $ addN (S (S O)) (S O)
-    print $ multN (S (S O)) (S (S (S O)))
-    print $ addP (T (T I)) (T I)
+    -- print $ addN (S (S O)) (S O)
+    -- print $ multN (S (S O)) (S (S (S O)))
+    -- print $ addP (T (T I)) (T I)
+
+    -- Integers: (II i j) represents i-j, (II k l) represents k-l
+    let i = 4
+    let j = 2
+    let k = 1
+    let l = 3
+
+    print $ int_ii (addI (II (nn_int i) (nn_int j)) (II (nn_int k) (nn_int l)))
+    print $ int_ii (multI (II (nn_int i) (nn_int j)) (II (nn_int k) (nn_int l)))
+
+    -- Fractions: (QQ i j) represents i/j, (QQ k l) represents k/l
+    -- print $ float_qq (addQ (QQ (ii_int i) (pp_int j)) (QQ (ii_int k) (pp_int l)))
+    -- print $ float_qq (multQ (QQ (ii_int i) (pp_int j)) (QQ (ii_int k) (pp_int l)))
+    
+    -- Normalisation (recursive definition)
+    print $ normalizeI (II (nn_int i) (nn_int j))
+
+    -- Normalisation (by evaluation)
+    print $ nbv (II (nn_int i) (nn_int j))
